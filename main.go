3e5b1770c935147c0339e897cc4b7e148b90773f
Alberto Morgante Medina <alknopfler@gmail.com>
2023-06-03 23:17:14 +0200

    add microphone lib to test it

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"github.com/MarkKremer/microphone"
+	"github.com/faiface/beep/wav"
 	openai "github.com/sashabaranov/go-openai"
+	"log"
 	"os"
+	"os/signal"
+	"strings"
 )
 
 const (
@@ -15,12 +19,51 @@ const (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("please provide a filename to convert to text")
+		fmt.Println("missing required argument: output file name")
 		return
 	}
-	if _, err := os.Stat(os.Args[1]); errors.Is(err, os.ErrNotExist) {
-		fmt.Printf("file %s does not exist\n", os.Args[1])
-		return
+	fmt.Println("Recording. Press Ctrl-C to stop.")
+
+	err := microphone.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer microphone.Terminate()
+
+	stream, format, err := microphone.OpenDefaultStream(sampleRate, seconds)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Close the stream at the end if it hasn't already been
+	// closed explicitly.
+	defer stream.Close()
+
+	filename := os.Args[1]
+	if !strings.HasSuffix(filename, ".wav") {
+		filename += ".wav"
+	}
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Stop the stream when the user tries to quit the program.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+	go func() {
+		<-sig
+		stream.Stop()
+		stream.Close()
+	}()
+
+	stream.Start()
+
+	// Encode the stream. This is a blocking operation because
+	// wav.Encode will try to drain the stream. However, this
+	// doesn't happen until stream.Close() is called.
+	err = wav.Encode(f, stream, format)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
@@ -28,7 +71,7 @@ func main() {
 		context.Background(),
 		openai.AudioRequest{
 			Model:    openai.Whisper1,
-			FilePath: os.Args[1],
+			FilePath: filename + ".wav",
 		},
 	)
 
